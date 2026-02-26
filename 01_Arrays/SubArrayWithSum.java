59ebf0bce6df2686ece4a17b6186424f82eb0a88
Your Name <your-email@example.com>
2026-02-26 15:53:09 +0530

    SubArray with the target sum

---
+++ 01_Arrays/SubArrayWithSum.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        
+        int a[]= new int [n];
+        
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int target= sc.nextInt();
+		
+		int left= 0;
+		int sum= 0;
+		boolean ans= false;
+		
+		for(int right=0;right<a.length;right++){
+		    sum+= a[right];
+		    while( sum > target && left <= right){
+		        sum-= a[left];
+		        left++;
+		    }
+		    
+		    if(sum == target ){
+		        ans= true;
+		        break;
+		    }
+		}
+		
+		if(ans){
+		    System.out.println("YES");
+		 }
+		 else{
+		    System.out.println("NO");
+		 }
+	}
+}
