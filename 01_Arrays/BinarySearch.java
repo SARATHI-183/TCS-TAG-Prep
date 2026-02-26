c89cf3ce5e69038a7038a4b25e97a8a69d9a9ba6
Your Name <your-email@example.com>
2026-02-26 16:45:29 +0530

    Binary search

---
+++ 01_Arrays/BinarySearch.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int a[]= new int [n];
+        
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        
+        int target= sc.nextInt();
+        int left= 0;
+        int right= n-1;
+        int res= -1;
+		
+		while( left <= right ){
+		    int mid = left + (right -left)/2;
+		    
+		    if(a[mid] == target){
+		        res= mid;
+		        break;
+		    }
+		    else if(a[mid] < target){
+		        left = mid + 1;
+		    }
+		    else{
+		        right = mid - 1;
+		    }
+		}
+		
+        System.out.println(res);
+	}
+}
