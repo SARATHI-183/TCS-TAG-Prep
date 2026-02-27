e03aaf99dc2ff5e9f6f5d114e97a5767b8bf08c8
Your Name <your-email@example.com>
2026-02-27 11:21:40 +0530

    Count the occurance of the element

---
+++ 01_Arrays/countOccuranceOfElement.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        // input size
+        int n = sc.nextInt();
+
+        // input array
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        // target element
+        int target = sc.nextInt();
+        
+        int first = findFirst(a, target);
+        int last = findLast(a, target);
+        
+        int count =0;
+        
+        if(first != -1){
+            count = last - first + 1;
+        }
+        
+        System.out.println(count);
+        sc.close();
+	}
+        
+    static int findFirst( int a[], int target){
+        int left= 0;
+        int right= a.length - 1;
+        int res = -1;
+        
+        while( left <= right ){
+            int mid = left + (right - left )/2;
+            
+            if( a[mid] == target ){
+                res = mid;
+                right = mid - 1;
+            }
+            
+            else if( a[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return res;
+    }
+        
+    static int findLast(int a[], int target){
+        int left= 0;
+        int right= a.length - 1;
+        int res = -1;
+        
+        while( left <= right ){
+            int mid = left + (right - left )/2;
+            
+            if( a[mid] == target ){
+                res = mid;
+                left = mid + 1;
+            }
+            
+            else if( a[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return res;
+    }
+}
