8773b55447458007e2b42ad8617eb534c9e37b1c
Your Name <your-email@example.com>
2026-02-27 15:45:24 +0530

    Search the insert index

---
+++ 01_Arrays/SearchInsertIndex.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        
+        int left= 0;
+        int right= n-1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(a[mid] == target){
+                System.out.println(mid);
+                sc.close();
+                return;
+            }
+            else if(a[mid] < target){
+                left= mid +1;
+            }
+            else{
+                right= mid -1;
+            }
+        }
+        System.out.println(left);
+        sc.close();
+	}
+}
